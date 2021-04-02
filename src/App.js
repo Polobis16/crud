48cf47a051882d5fbfe284fc8601c2ecd33576db
Polobis16 <emmamonier@outlook.com>
2021-04-01 21:54:25 -0300

    tutorial16

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
@@ -14,12 +14,17 @@ function App() {
     }
 
     const newTask = {
-      id: shortid.generate,
+      id: shortid.generate(),
       name: task
     }
 
-    setTasks([ ...task, newTask])
+    setTasks([ ...tasks, newTask ])
     setTask("")
+  }
+
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter(task => task.id !== id)
+    setTasks(filteredTask)
   }
 
   return (
@@ -29,17 +34,32 @@ function App() {
       <div className="row">
         <div className ="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
-          <ul className="list-group">
-            { 
-              tasks.map((task) => (
-                <li className="list-group-item" key={task.id}>
-                <span className="lead">{task.name}</span>
-                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                <button className="btn btn-warning btn-sm float-right">Editar</button>
-                </li>
-              ))
-            }
-          </ul>
+          {
+            size(tasks) === 0 ? (
+              <h5 className="text-center">Aún no hay Tareas Programadas</h5>
+            ) : (
+              <ul className="list-group">
+              { 
+                tasks.map((task) => (
+                  <li className="list-group-item" key={task.id}>
+                    <span className="lead">{task.name}</span>
+                    <button 
+                      className="btn btn-danger btn-sm float-right mx-2"
+                      onClick={() => deleteTask(task.id)}
+                    >
+                      Eliminar
+                    </button>
+                    <button 
+                      className="btn btn-warning btn-sm float-right"
+                    >
+                      Editar
+                    </button>
+                  </li>
+                ))
+              }
+              </ul>
+            )
+          }
         </div>
         <div className ="col-4">
         <h4 className="text-center">Formulario</h4>
